94df6bae6415d5831f91614bd0c4085f7e1da9ad
phanick <39258517+phanick@users.noreply.github.com>
2022-03-17 17:34:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
   # Create AWS ec2 instance
   resource "aws_instance" "ubuntu-tf" {
   ami = var.ami_id
-  vpc_id = "vpc-0043430975cdcbb92"
+  # vpc_id = "vpc-0043430975cdcbb92"
   key_name = var.key_name
   instance_type = var.instance_type
   # security_groups= var.security_group
