5df3f3e04d8dbb9dbe34e422460cf78982ac8143
phanick <39258517+phanick@users.noreply.github.com>
2022-03-17 17:33:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
   vpc_id = "vpc-0043430975cdcbb92"
   key_name = var.key_name
   instance_type = var.instance_type
-  security_groups= var.security_group
+  # security_groups= var.security_group
   tags= {
     Name = var.tag_name
   }
